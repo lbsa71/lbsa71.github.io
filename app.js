013d86a999d7d212d97e27d9e93f70f7e9401fe8
Stefan Andersson <stefan.andersson@kaustik.com>
2019-09-08 11:27:25 +0200

    Added timer update

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
-var start = new Date()
-var end = new Date(2029, 7, 1)
-
-var start_txt = start.toISOString()
-var end_txt = end.toISOString()
-
-var today = new Date(start.getFullYear(), start.getMonth(), start.getDay(), 0,0,0,0)
-var today_txt = end.toISOString()
-
-var today_ms = today.getTime()
-var end_ms = end.getTime()
-
+function calculate() {
 const ms_per_day = 1000 * 3600 * 24
 const vacation_weeks = [1, 27, 28, 29, 30, 52, 53]
 
-var days_ms = end_ms-today_ms
+var now = new Date()
+var now_day = new Date(now.getFullYear(), now.getMonth(), now.getDay(), 0,0,0,0)
+
+var start = new Date(1990,8,1)
+var start_day = new Date(start.getFullYear(), start.getMonth(), start.getDay(), 0,0,0,0)
+var start_day_ms = start_day.getTime()
+var start_txt = start.toISOString()
+
+var end_day = new Date(2029, 7, 1)
+var end_day_ms = end_day.getTime()
+var end_day_txt = end_day.toISOString()
+
+var today_txt = now.toISOString()
+
+var days_ms = end_day_ms-start_day_ms
 var days = days_ms / ms_per_day
 
 var workdays = 0
 
+var days_until_now;
+
 for(d = 0; d < days; d++)
 {
   var is_workday = true
-  var as_ms = today_ms + d * ms_per_day
+  var as_ms = start_day_ms + d * ms_per_day
   var as_date = new Date(as_ms)
+
+  if(as_date < now_day) days_until_now = workdays;
 
   var day = as_date.getDay()
 
@@ -38,4 +45,8 @@ for(d = 0; d < days; d++)
   if(vacation_weeks.includes(week)) is_workday = false
 
   if(is_workday) workdays++
+}
+var now_to_end_days = workdays - days_until_now
+
+ return { today_txt, start_txt, end_day_txt, days, workdays, now_to_end_days }
 }
