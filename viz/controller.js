0b49633d78dccf26333c86e349c18591dbdbf34e
Stefan Andersson <stefan.andersson@kaustik.com>
2020-04-08 12:48:26 +0200

    Added swedish total deaths per month 2019

--- viz/controller.js
+++ viz/controller.js
@@ -1,7 +1,113 @@
 var vlSpec = {
   $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+  resolve: {
+    axis: {
+      x: "shared"
+    }
+  },
+  layer: [{
+      data: {
+        name: "deathsPerDaySweden",
+        url: "data/deathsPerDaySweden.tsv",
+        format: {
+          type: "tsv",
+          parse: {
+            dateRep: "date:'%m/%d/%Y'"
+          }
+        }
+      },
+      transform: [{
+        calculate: "datum.deaths * 10000000 / datum.popData2018",
+        as: "deathsPer10M"
+      }],
+      mark: {
+        type: "line",        
+        clip: "true",
+        interpolate: "basis"
+      },
+      encoding: {
+        y: {
+          field: "Dead",
+          type: "quantitative"
+        }
+      }
+    },
+    {
+      data: {
+        name: "raw",
+        url: "data/covid-19.csv",
+        // url: "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv",
+        format: {
+          type: "csv",
+          parse: {
+            dateRep: "date:'%d/%m/%Y'"
+          }
+        }
+      },
+      transform: [{
+          filter: {
+            field: "countriesAndTerritories",
+            oneOf: ["Sweden", "Italy", "Norway", "China", "United_States_of_America"]
+          }
+        },
+        {
+          calculate: "datum.deaths * 10000000 / datum.popData2018",
+          as: "deathsPer10M"
+        },
+      ],
+      mark: {
+        type: "line",
+        clip: "true",
+        interpolate: "basis"
+      },
+      encoding: {
+        color: {
+          field: 'countriesAndTerritories',
+          type: 'nominal'
+        },
+        y: {
+          field: "deathsPer10M",
+          type: "quantitative",
+          axis: {
+            title: 'Deaths'
+          }
+        }
+      }
+    }
+  ],
 
-  data: {
+  encoding: {
+    x: {
+      field: 'dateRep',
+      type: 'temporal',
+      axis: {
+        title: 'Date'
+      },
+      scale: {
+        domain: [{
+            year: 2020,
+            month: 3
+          },
+          {
+            year: 2020,
+            month: 6
+          }
+        ]
+      }
+    },
+  }
+}
+
+// ---
+
+var vlSpec2 = {
+  $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+  resolve: {
+    axis: {
+      x: "shared"
+    }
+  },
+  data: [{
     name: "raw",
     url: "data/covid-19.csv",
     // url: "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv",
@@ -11,8 +117,28 @@ var vlSpec = {
         dateRep: "date:'%d/%m/%Y'"
       }
     }
-  },
+  }],
   layer: [{
+      data: {
+        name: "deathsPerDaySweden",
+        url: "data/deathsPerDaySweden.tsv",
+        format: {
+          type: "tsv",
+          parse: {
+            dateRep: "date:'%m/%d/%Y'"
+          }
+        }
+      },
+      mark: "line",
+      encoding: {
+
+        y: {
+          field: "Dead",
+          type: "quantitative"
+        }
+      }
+    },
+    {
       mark: "line",
       encoding: {
         y: {
@@ -72,16 +198,16 @@ var vlSpec = {
       as: "newDeathsPer10M"
     },
     {
-      filter:
-      {
-        field: "countriesAndTerritories", "oneOf": ["Sweden", "Italy"]
+      filter: {
+        field: "countriesAndTerritories",
+        oneOf: ["Sweden", "Italy", "Norway "]
       }
     },
     {
       filter: "datum.cases > 0"
     },
     {
-      groupby: [ "countriesAndTerritories" ],
+      groupby: ["countriesAndTerritories"],
       sort: [{
         "field": "dateRep"
       }],
